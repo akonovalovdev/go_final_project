baaf9c5bc5854cd4ecb3f3edaba7c124e11c02c3
akonovalov <akonovalov.dev@gmail.com>
2024-04-20 18:04:17 +0500

    Adds dimple server

--- main.go
+++ main.go
@@ -1,2 +1,22 @@
 package main
 
+import (
+	"net/http"
+	"os"
+	"log"
+)
+
+func main() {
+	port := os.Getenv("TODO_PORT")
+	if port == "" {
+		port = "7540"
+	}
+
+	http.Handle("/", http.FileServer(http.Dir("./web")))
+
+	log.Printf("Server starting on port %s\n", port)
+	err := http.ListenAndServe(":" + port, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
