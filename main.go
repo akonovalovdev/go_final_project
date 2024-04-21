8623441f2038c6949d6ab46a86264cf657dbd06d
akonovalov <akonovalov.dev@gmail.com>
2024-04-21 19:44:04 +0500

    Adds method new task

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"log"
@@ -14,6 +15,13 @@ import (
 
 	_ "modernc.org/sqlite"
 )
+
+type Task struct {
+	Date    string `json:"date"`
+	Title   string `json:"title"`
+	Comment string `json:"comment"`
+	Repeat  string `json:"repeat"`
+}
 
 func main() {
 	dbFilePath := os.Getenv("TODO_DBFILE")
@@ -83,6 +91,63 @@ func main() {
 		}
 
 		w.Write([]byte(nextDate))
+	})
+
+	http.HandleFunc("/api/task", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodPost:
+			var task Task
+			err := json.NewDecoder(r.Body).Decode(&task)
+			if err != nil {
+				http.Error(w, `{"error":"Invalid request body"}`, http.StatusBadRequest)
+				return
+			}
+
+			if task.Title == "" {
+				http.Error(w, `{"error":"Title is required"}`, http.StatusBadRequest)
+				return
+			}
+
+			if task.Date != "" {
+				_, err = time.Parse("20060102", task.Date)
+				if err != nil {
+					http.Error(w, `{"error":"Invalid date format"}`, http.StatusBadRequest)
+					return
+				}
+			}
+
+			if task.Date == "" || task.Date < time.Now().Format("20060102") {
+				task.Date = time.Now().Format("20060102")
+			}
+
+			if task.Repeat == "d 1" {
+				task.Date = time.Now().Format("20060102")
+			} else if task.Repeat != "" {
+				task.Date, err = NextDate(time.Now(), task.Date, task.Repeat)
+				if err != nil {
+					http.Error(w, `{"error":"Invalid repeat rule"}`, http.StatusBadRequest)
+					return
+				}
+			}
+
+			res, err := db.Exec(`INSERT INTO scheduler (date, title, comment, repeat) VALUES (?, ?, ?, ?)`,
+				task.Date, task.Title, task.Comment, task.Repeat)
+			if err != nil {
+				http.Error(w, `{"error":"Failed to insert task"}`, http.StatusInternalServerError)
+				return
+			}
+
+			id, err := res.LastInsertId()
+			if err != nil {
+				http.Error(w, `{"error":"Failed to get task ID"}`, http.StatusInternalServerError)
+				return
+			}
+
+			w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+			json.NewEncoder(w).Encode(map[string]string{"id": strconv.FormatInt(id, 10)})
+		default:
+			http.Error(w, `{"error":"Invalid method"}`, http.StatusMethodNotAllowed)
+		}
 	})
 
 	log.Printf("Server starting on port %s\n", port)
